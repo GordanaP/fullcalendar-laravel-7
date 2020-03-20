145ea7948f05d219f90051d6310c14e6cdce9bc5
Gordana <minjon56@gmail.com>
2020-03-20 11:34:54 +0100

    Initial settings

---
+++ resources/views/partials/_head.blade.php
@@ -0,0 +1,26 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Fullcalendar -->
+<link href="{{ asset('vendor/fullcalendar-4.3.1/packages/core/main.css') }}" rel='stylesheet' />
+<link href="{{ asset('vendor/fullcalendar-4.3.1/packages/daygrid/main.css') }}" rel='stylesheet' />
+<link href="{{ asset('vendor/fullcalendar-4.3.1/packages/timegrid/main.css') }}" rel='stylesheet' />
+<link href="{{ asset('vendor/fullcalendar-4.3.1/packages/list/main.css') }}" rel='stylesheet' />
+
+<!-- Datepicker -->
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/jquery-ui-1.12.1.purple/jquery-ui.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/jquery-ui-1.12.1.purple/jquery-ui.structure.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/jquery-ui-1.12.1.purple/jquery-ui.theme.css') }}">
+
+<!-- Timepicker -->
+<link rel="stylesheet" href="{{ asset('vendor/jonthornton-jquery-timepicker/jquery.timepicker.css') }}">
+
+<!-- App styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+@yield('links')
---
+++ resources/views/partials/_scripts.blade.php
@@ -0,0 +1,52 @@
+<script src="{{ asset('js/app.js') }}" ></script>
+
+<!-- Moment -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="{{ asset('vendor/moment-2.24.0/moment.min.js') }}"></script>
+
+
+<!-- Moment timezone -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.28/moment-timezone.min.js"></script>
+{{-- <script src="{{ asset('vendor/moment-timezone-0.5.28/moment-timezone.min.js') }}"></script> --}}
+
+<!-- Moment plugin to make date range -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-range/4.0.2/moment-range.js" integrity="sha256-bB6c2ZfNzG6Tv8MSu/pqUl0y91h86M/T+1w0WrCZhGw=" crossorigin="anonymous"></script>
+{{-- <script src="{{ asset('vendor/moment-range-master/lib/moment-range.js') }}"></script> --}}
+
+<!-- Moment plugin to calculate dates for weekdays -->
+<script src="{{ asset('vendor/moment-weekday-calc/src/moment-weekday-calc.js') }}"></script>
+
+<script>
+    window['moment-range'].extendMoment(moment);
+</script>
+
+<!-- FullCalendar -->
+<script src="{{ asset('vendor/fullcalendar-4.3.1/packages/core/main.js') }}"></script>
+<script src="{{ asset('vendor/fullcalendar-4.3.1/packages/interaction/main.js') }}"></script>
+<script src="{{ asset('vendor/fullcalendar-4.3.1/packages/daygrid/main.js') }}"></script>
+<script src="{{ asset('vendor/fullcalendar-4.3.1/packages/timegrid/main.js') }}"></script>
+<script src="{{ asset('vendor/fullcalendar-4.3.1/packages/list/main.js') }}"></script>
+
+<!-- Datepicker -->
+<script src="{{ asset('vendor/jquery-ui-1.12.1.purple/jquery-ui.js') }}"></script>
+
+<!-- Timepicker -->
+<script src="{{ asset('vendor/jonthornton-jquery-timepicker/jquery.timepicker.js') }}"></script>
+
+@yield('scripts')
+
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        statusCode: {
+            401: function() {
+                redirectTo("{{ route('login') }}")
+            },
+            403: function() {
+                alert('The action is unauthorized');
+            }
+        }
+    });
+</script>
